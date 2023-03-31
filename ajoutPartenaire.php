7fbe20272b74c71f6d8b406dd82d6c8d2f754cbd
Fabien Rozier <fabienrozier60@gmail.com>
2023-03-31 15:15:29 +0200

    Changement Nom Img pour ajout + modif et Modif 100%

--- ajoutPartenaire.php
+++ ajoutPartenaire.php
@@ -18,7 +18,10 @@ if(isset($_POST['nompart'], $_POST['descrippart'], $_POST['lienpart'])){
 
         if (isset($_FILES['imgpart'])) {
             $nom_image = $_FILES['imgpart']['name'];
-            $nom_image = rand(1000,9999)."-".$nom_image;
+            $nomexplode = explode(".",$nom_image);
+            $ext = end($nomexplode);
+            $nom_image = substr($nom_image,0,-(strlen($ext)+1));
+            $nom_image .= "-".rand(1000,9999).".".$ext;
             move_uploaded_file($_FILES['imgpart']['tmp_name'], 'assets/' . $nom_image);
             $InsertImg = $connexion->prepare("INSERT INTO image (Nom_Image) VALUES (:nom)");
             $InsertImg->bindParam("nom",$nom_image);
--- modifPartenaire.php
+++ modifPartenaire.php
@@ -10,7 +10,8 @@ if(isset($_POST['idPart'], $_POST['nompart'], $_POST['descrippart'], $_POST['lie
         $nom_image = $_FILES['imgpart']['name'];
         $nomexplode = explode(".",$nom_image);
         $ext = end($nomexplode);
-        $nom_image .= "-".rand(1000,9999).$ext;
+        $nom_image = substr($nom_image,0,-(strlen($ext)+1));
+        $nom_image .= "-".rand(1000,9999).".".$ext;
         move_uploaded_file($_FILES['imgpart']['tmp_name'], 'assets/' . $nom_image);
         $SelectLastImg = $connexion->prepare("SELECT Id_Image FROM partenaire WHERE Id_Partenaire = :idpart ");
         $SelectLastImg->bindParam("idpart",$id);
@@ -27,22 +28,18 @@ if(isset($_POST['idPart'], $_POST['nompart'], $_POST['descrippart'], $_POST['lie
                 unlink('assets/'.$LastnomImg['Nom_Image']);
             }catch(Exception $e){
             }
-            $InsertImg = $connexion->prepare("INSERT INTO image (Nom_Image) VALUES (:nom)");
+            $InsertImg = $connexion->prepare("UPDATE image SET Nom_Image = :nom WHERE Id_Image = :idimg");
             $InsertImg->bindParam("nom",$nom_image);
+            $InsertImg->bindParam("idimg",$LastImg['Id_Image']);
             if($InsertImg->execute() === false){
                 echo "Erreur lors de l'insertion de l'image";
             }else{
-                $idimg = $connexion->lastInsertId();
-                $req = $connexion->prepare("UPDATE partenaire SET Nom_Partenaire = :nom, Description_Partenaire = :descrip, Lien_Partenaire = :lien, Id_Image = :idimg WHERE Id_Partenaire = :id");
+                $req = $connexion->prepare("UPDATE partenaire SET Nom_Partenaire = :nom, Description_Partenaire = :descrip, Lien_Partenaire = :lien WHERE Id_Partenaire = :id");
                 $req->bindParam("id",$id);
                 $req->bindParam("nom",$nom);
                 $req->bindParam("descrip",$descrip);
                 $req->bindParam("lien",$lien);
-                $req->bindParam("idimg",$idimg);
                 if($req->execute()){
-                    $DeleteLastImg = $connexion->prepare("DELETE FROM image WHERE Id_Image = :idimg ");
-                    $DeleteLastImg->bindParam("idimg",$LastImg['Id_Image']);
-                    $DeleteLastImg->execute();
                     echo "Le partenaire ".$nom." a bien été modifié.";
                 }else{
                     echo "Error: " . $req . "<br>" . $connexion->error;
