27cef09faa0b0554126b7b66366befb359aeb9c1
Lucas Dupas <dupaslucas8@gmail.com>
2023-04-03 15:54:06 +0200

    Passage de l'index en php/ MAJ css/ Nouvel image

---
+++ index.php
@@ -0,0 +1,195 @@
+<?php 
+ require 'include/connexion_db.php';
+
+ //Récupération des données de Téléphone
+$tel = $connexion -> prepare('SELECT Num_TEL_Info_Accueil FROM info_accueil');
+$tel ->execute();
+$phone = $tel->fetch();
+$phone = $phone['Num_TEL_Info_Accueil'];
+
+//Récpération des données de l'email
+$email = $connexion -> prepare('SELECT Email_Info_Accueil FROM info_accueil');
+$email ->execute();
+$adresseEmail = $email->fetch();
+$adresseEmail = $adresseEmail['Email_Info_Accueil'];
+
+//Récupération des données de l'emplacement du bureau du CSE
+$place = $connexion -> prepare('SELECT Emplacement_Bureau_Info_Accueil FROM info_accueil');
+$place ->execute();
+$office = $place->fetch();
+$office = $office['Emplacement_Bureau_Info_Accueil'];
+
+//Images partenaires
+$imgPart = $connexion -> prepare("SELECT DISTINCT * FROM image ORDER BY RAND() LIMIT 3");
+$imgPart -> execute();
+$nomImg = $imgPart->fetchAll();
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap" rel="stylesheet">
+    <link rel="icon" href="assets/sv_logo.png">
+    <title>CSE Saint-Vincent - Accueil</title>
+</head>
+
+<body>
+    <header>
+        <div class="gris">
+
+        </div>
+        <div class="blue">
+            <nav>
+                <div class="logo"><img src="assets/logo_lycee.png" alt="logo_st_vincent"></div>
+                <ul>
+                    <a href="index.php">
+                        <li class="active">
+                            Accueil
+                        </li>
+                    </a>
+                    <a href="partenariats.html">
+                        <li>Partenariats</li>
+                    </a>
+                    <a href="billetterie.html">
+                        <li>Billetterie</li>
+                    </a>
+                    <a href="contact.html">
+                        <li>Contact</li>
+                    </a>
+                </ul>
+
+            </nav>
+        </div>
+    </header>
+
+    <main>
+        <aside class="left">
+            <div class="home">
+                <img src="assets/homeIcon.png">
+                <img src="assets/chevron-droit.png" class="chevron-droit">
+                <a href="index.php" class="sectiontitle">Accueil</a>
+            </div>
+            <div class="fastaccess">
+                <h1 class="sectiontitle">Accès rapide</h1>
+                <div class="offreaccess">
+                    <img src="assets/chevron-droit.png" class="chevron-droit">
+                    <a href="billetterie.html">Offres / Billeterie</a>
+                </div>
+                <div class="contactaccess">
+                    <img src="assets/chevron-droit.png" class="chevron-droit">
+                    <a href="contact.html">Nous contacter</a>
+                </div>
+            </div>
+            <div class="infocontact">
+                <h1 class="sectiontitle">Informations de contact</h1>
+                <div class="tel">
+                    <p><img src="assets/chevron-droit.png" class="chevron-droit">
+                        Par téléphone : <a target="_blank" href="tel:+3330303030303">+<?=$phone ?></a>
+                    </p>
+                </div>
+                <div class="email">
+                    <p><img src="assets/chevron-droit.png" class="chevron-droit">
+                        Par email : <a target="_blank"
+                            href="mailto:cse@lyceestvincent.fr"><?= $adresseEmail ?></a>
+                    </p>
+                </div>
+                <div class="lieu">
+                    <p><img src="assets/chevron-droit.png" class="chevron-droit">
+                        Au lycée : <a target="_blank"
+                            href="https://www.google.com/maps/place/49%C2%B012'08.1%22N+2%C2%B035'18.9%22E/@49.202244,2.5880054,19z/data=!3m1!4b1!4m4!3m3!8m2!3d49.202244!4d2.58857"><?= $office ?></a>
+                    </p>
+                </div>
+            </div>
+            <div class="partenaire">
+                <h1 class="sectiontitle">Nos partenaires</h1>
+                <div class="boxSlider">
+                    <div class="slideshow-container">
+                        <?php foreach($nomImg as $image ){?>
+                        <div class="mySlides">
+
+                                <img src="<?php echo "assets/".$image['Nom_Image']."" ?>">
+                        </div>
+                        <?php } ?>
+                    </div>
+                    <img class="prev" onclick="plusSlides(-1)" src="assets/chevron-gauche.png">
+                    <img class="next" onclick="plusSlides(1)" src="assets/chevron-droit.png">
+                    <div style="text-align:center">
+                        <span class="dot" onclick="currentSlide(1)"></span>
+                        <span class="dot" onclick="currentSlide(2)"></span>
+                        <span class="dot" onclick="currentSlide(3)"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="decouverte">
+                <a target="_blank" href="partenariats.html">Découvrir tous nos partenaires</a>
+            </div>
+        </aside>
+        <div class="right">
+            <div class="info_accueil">
+                <h2 class="titre">CSE Lycée Saint-Vincent</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam quas veritatis mollitia libero
+                    provident eius. <br>Perferendis eos aperiam pariatur accusantium minus quo vitae! Totam voluptate
+                    soluta necessitatibus harum praesentium officia.</p>
+            </div>
+            <h1>Dernières offres de la Billetterie</h1>
+            <div class="offre_billetterie">
+                <div class="offre_billetterie_header">
+                    <span class="tag_offre">OFFRE</span>
+                    <span class="date_offre">Publiée le 11 septembre 2001</span>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima, delectus et numquam amet quis
+                    cumque natus sunt quos odit similique itaque enim sint atque, mollitia quidem officia a nulla! Vel.
+                </p>
+                <a href="">
+                    <span class="offre_learnmore">EN SAVOIR PLUS <img class="chevron-droit"
+                            src="assets/chevron-droit.png" alt="chevron-droit"></span>
+                </a>
+            </div>
+            <a target="_blank" href="billetterie.html">
+                <span id="offres_decouvrir">Découvrir toutes nos offres 〉</span>
+            </a>
+        </div>
+
+    </main>
+    <footer>
+        <div class="left_footer">
+            <div class="logo_footer">
+                <img src="assets/logo_lycee.png" alt="logo_st_vincent">
+            </div>
+        </div>
+        <div class="right_footer">
+            <div class="title_footer">
+                <h1><strong> CSE Lycée Saint-Vincent</strong></h1>
+            </div>
+            <div class="links_footer">
+                <ul class="links_list_footer">
+                    <a href="partenariats.html">
+                        <li><img src="assets/chevron-droit.png" class="chevron-droit" alt=""> Partenariats</li>
+                    </a>
+                    <a href="billetterie.html">
+                        <li><img src="assets/chevron-droit.png" class="chevron-droit" alt=""> Billeterie</li>
+                    </a>
+                    <a href="contact.html">
+                        <li><img src="assets/chevron-droit.png" class="chevron-droit" alt=""> Contact</li>
+                    </a>
+                </ul>
+            </div>
+        </div>
+    </footer>
+    <script src="scriptaside.js"></script>
+</body>
+
+</html>
