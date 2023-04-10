571fd364a11718b3bb3cac7a589d7e2cdba1eca3
Lucas Dupas <dupaslucas8@gmail.com>
2023-04-10 18:15:40 +0200

    lien actifs header

--- include/header.php
+++ include/header.php
@@ -1,3 +1,27 @@
+<?php
+$parts = explode('/', $_SERVER["SCRIPT_NAME"]);
+$file = $parts[count($parts) - 1];
+$link_index ="";
+$link_partenaires ="";
+$link_billetterie ="";
+$link_contact ="";
+
+//var_dump($file);
+// Conditions si le lien fini par... alors le lien prends la valeur active
+if($file === "index.php"){
+    $link_index = 'active';
+}
+if($file === "partenariats.php"){
+    $link_partenaires = 'active';
+}
+if($file == "billetterie.php"){
+    $link_billetterie = 'active';
+}
+if($file == "contact.php"){
+    $link_contact = 'active';
+}
+
+?>
 <header>
     <div class="gris">
     </div>
@@ -6,18 +30,18 @@
             <div class="logo"><img src="assets/logo_lycee.png" alt="logo_st_vincent"></div>
             <ul>
                 <a href="index.php">
-                    <li class="active">
+                    <li class="<?= $link_index ?>">
                         Accueil
                     </li>
                 </a>
                 <a href="partenariats.php">
-                    <li>Partenariats</li>
+                    <li class="<?= $link_partenaires ?>">Partenariats</li>
                 </a>
                 <a href="billetterie.php">
-                    <li>Billetterie</li>
+                    <li class="<?= $link_billetterie ?>">Billetterie</li>
                 </a>
                 <a href="contact.php">
-                    <li>Contact</li>
+                    <li class="<?= $link_contact ?>">Contact</li>
                 </a>
             </ul>
 
