1d2d0626515bc38e785be50c8f36a4b29d433a2d
Raphaël TOURSEL <thewhiteshadowgaming@gmail.com>
2023-04-11 11:23:07 +0200

    Modif connexion pour bdd en ligne

--- include/connexion_db.php
+++ include/connexion_db.php
@@ -1,7 +1,10 @@
 <?php
     try{
-        $dbname = "cse_lsv";
-        $connexion= new PDO('mysql:host=localhost;dbname='.$dbname.'', 'root', '');
+        $dbname = "lyceestvincent_csebts1g1";
+        $host = "mysql-lyceestvincent.alwaysdata.net";
+        $user = "116313_csebts1g1";
+        $mdp = "CSE!TDR";
+        $connexion= new PDO('mysql:host='.$host.';dbname='.$dbname.'', $user, $mdp);
     }catch(Exception $e){
         echo "Erreur de connexion.";
     }
