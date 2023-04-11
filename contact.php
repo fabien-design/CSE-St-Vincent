ed90c46d8ad78b605d941bab94e689eca58d45d0
Raphaël TOURSEL <thewhiteshadowgaming@gmail.com>
2023-04-11 09:04:15 +0200

    Ajout contact et style de contact

--- contact.php
+++ contact.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="styleContact.css">
     <link
         href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
@@ -22,6 +23,26 @@
         <?php require 'include/aside.php'?>
         <div class="right">
             <h1>Page de contact</h1>
+
+            <section class="contact" id="linkToContact">
+		<div class="contactForm">
+				<form action="">
+					<label for="fname">Nom</label>
+					<input type="text" id="fname" name="firstname" placeholder="Votre Nom">
+				
+                    <label for="lname">Prénom</label>
+					<input type="text" id="lname" name="lastname" placeholder="Votre Prénom"> 
+
+					<label for="lname">Email</label>
+					<input type="text" id="lname" name="lastname" placeholder="Votre adresse Email"> 
+
+					<label for="subject">Contenu</label>
+					<textarea id="subject" name="subject" placeholder="Saisir votre message" style="height:200px"></textarea>
+				
+					<input type="submit" value="Soumettre">
+				  </form>
+		</div>
+	</section>
         </div>
     </main>
     <?php require 'include/footer.php'?>
