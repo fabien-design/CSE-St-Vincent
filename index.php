59e2e2184f0ce5882a0934afed30a4c6b17bd8c8
Lucas Dupas <dupaslucas8@gmail.com>
2023-04-24 19:42:53 +0200

    oui

--- index.php
+++ index.php
@@ -55,7 +55,8 @@ setlocale(LC_TIME,"fr_FR.utf8");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="responsive.css">
+    <link rel="stylesheet" href="responsive_index.css">
+    <link rel="stylesheet" href="responsive_all.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
