03b20e61e7a14612858e2288fbf61fe87d044895
Lucas Dupas <dupaslucas8@gmail.com>
2023-04-24 19:41:55 +0200

    Debut responsive

--- contenu_offre_billetterie.php
+++ contenu_offre_billetterie.php
@@ -38,6 +38,7 @@ $link = $modalLink->fetch();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="responsive.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
--- partenariats.php
+++ partenariats.php
@@ -26,6 +26,7 @@ $idPartenaire = $req->fetchAll();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="stylePartenariats.css">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="responsive.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
