79482215649e16e6f8140f0defad6d09297c67b5
Raphaël TOURSEL <thewhiteshadowgaming@gmail.com>
2023-05-22 10:21:16 +0200

    Fix extra chevron

--- include/aside.php
+++ include/aside.php
@@ -38,7 +38,7 @@ $nomImg = $imgPart->fetchAll();
                    
                 ?>
                 <img src="assets/homeIcon.png" alt="homeIcon.png">
-                <img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">>
+                <img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">
                 <h1 class="sectiontitle">Accueil</h1>
                 <?php }?>
                 <?php
@@ -46,7 +46,7 @@ $nomImg = $imgPart->fetchAll();
                     
                 ?>
                 <img src="assets/partenaireIcon.png" alt="partenaireIcon.png">
-                <img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">>
+                <img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">
                 <h1 class="sectiontitle">Partenariats</h1>
                 <?php }?>
                 <?php
@@ -54,7 +54,7 @@ $nomImg = $imgPart->fetchAll();
                     
                 ?>
                 <img src="assets/billetIcon.png" alt="billetIcon.png">
-                <img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">>
+                <img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">
                 <h1 class="sectiontitle">Billetterie</h1>
                 <?php }?>
                 <?php
@@ -62,7 +62,7 @@ $nomImg = $imgPart->fetchAll();
                    
                 ?>
                 <img src="assets/contactsIcon.png" alt="contactsIcon.png">
-                <img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">>
+                <img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">
                 <h1 class="sectiontitle">Contact</h1>
                 <?php }?>
                 
@@ -70,28 +70,28 @@ $nomImg = $imgPart->fetchAll();
             <div class="fastaccess">
                 <h1 class="sectiontitle">Accès rapide</h1>
                 <div class="offreaccess">
-                    <img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">>
+                    <img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">
                     <a href="billetterie.php?page=1">Offres / Billetterie</a>
                 </div>
                 <div class="contactaccess">
-                    <img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">>
+                    <img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">
                     <a href="contact.php">Nous contacter</a>
                 </div>
             </div>
             <div class="infocontact">
                 <h1 class="sectiontitle">Informations de contact</h1>
                 <div class="tel">
-                    <p><img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">>Par téléphone : <a target="_blank" href="tel:+3330303030303">+<?=$phone ?></a>
+                    <p><img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">Par téléphone : <a target="_blank" href="tel:+3330303030303">+<?=$phone ?></a>
                     </p>
                 </div>
                 <div class="email">
                     <p>
-                        <img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">>Par email : <a target="_blank"
+                        <img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">Par email : <a target="_blank"
                             href="mailto:cse@lyceestvincent.fr"><?= $adresseEmail ?></a>
                     </p>
                 </div>
                 <div class="place">
-                    <p><img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">>Au lycée : <a target="_blank"
+                    <p><img src="assets/chevron-droit.png" class="chevron-droit" alt="chevron-droit">Au lycée : <a target="_blank"
                             href="https://www.google.com/maps/place/49%C2%B012'08.1%22N+2%C2%B035'18.9%22E/@49.202244,2.5880054,19z/data=!3m1!4b1!4m4!3m3!8m2!3d49.202244!4d2.58857"><?= $office ?></a>
                     </p>
                 </div>
